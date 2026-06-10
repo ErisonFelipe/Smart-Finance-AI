864e066ea790606a35a4c6e92b084e4459ffa92f
Erison Felipe <erisonfelipe1@outlook.com>
2026-06-10 11:37:27 -0300

    fix: adicionar /api na baseURL

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = "https://smart-finance-ai-backend-production.up.railway.app/";
+const baseURL = "https://smart-finance-ai-backend-production.up.railway.app/api";
 
 const api = axios.create({ baseURL });
 
